65bd334b3118a96c184f29c13ae853309ecb6ab8
raissa620-d <deborahdzahini2@gmail.com>
2025-11-24 13:13:47 +0000

    véhicules

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.accueil, name='accueil'),
+   path('véhicules/', views.liste_véhicules, name='liste_véhicules'),
 ]
+
--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
-
-
-# Create your views here.
 from django.shortcuts import render
 from .models import Vehicule
 
 def accueil(request):
     vehicules = Vehicule.objects.filter(statut='disponible')
     return render(request, 'core/accueil.html', {'vehicules': vehicules})
+
+def liste_véhicules(request):
+    return render(request, 'core/liste_véhicules.html')
